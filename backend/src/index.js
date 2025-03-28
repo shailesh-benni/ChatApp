e332a5fa8fd832454635ba30f76bd2b154453f2c
testing-gems <shailesh.butter@gmail.com>
2025-03-28 10:26:51 +0530

    Message Controller and Routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "../routes/auth.routes.js"
+import authRoutes from "../routes/auth.routes.js";
+import messageRoutes from "../routes/message.routes.js";
+
 import { connectDB } from "../lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -16,7 +18,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message",messageRoutes);
+
+
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
     connectDB();
