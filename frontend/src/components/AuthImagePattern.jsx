de4be18a5baf3fdf8428116e814384d97a67b9ae
testing-gems <shailesh.butter@gmail.com>
2025-04-08 16:07:08 +0530

    Boxes

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,19 +1,20 @@
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 my">
-      <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-  }}>
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-      <p className="mb-5">{subtitle}</p>
-    </div>
-  </div>
-</div>
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 ">
+      <div className="max-w-sm text-center">
+        <div className="grid grid-cols-3 gap-3 mb-8 mt-10">
+          {[...Array(9)].map((_, i) => (
+            <div
+              key={i}
+              className={`aspect-square rounded-xl bg-primary ${
+                i % 2 === 0 ? "animate-pulse" : ""
+              }`}
+            />
+          ))}
+        </div>
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
     </div>
   );
 };
